f55d7a4b75c43d93cb541f203492b9e889c24418
Rahul Sekhar <sekhar.rahul@gmail.com>
2015-01-25 10:22:25 +0530

    fix layout

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,5 +1,5 @@
 class Admin::SessionsController < ::Devise::SessionsController
-  layout "admin/application"
+  layout 'admin'
 
   def after_sign_in_path_for(admin)
     session.delete(:admin_previous_url) || admin_root_path
